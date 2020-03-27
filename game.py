ce3e229f92bfdd82844a1d6f86377622161170d3
Dmitry Granovsky <dima.granovsky@gmail.com>
2020-03-27 19:51:19 +0300

    add /go command

--- game.py
+++ game.py
@@ -1,9 +1,26 @@
-class Game():
-    def __init__(self):
-        self.players = set()
+MIN_PLAYERS = 1
 
-    def add_player(self, player_id):
-        if player_id in self.players:
+class Game():
+
+    def __init__(self):
+        self.players = {}
+        self.started = False
+        self.rounds = 0
+
+    def add_player(self, player):
+        if player.id in self.players:
             return False
-        self.players.add(player_id)
+        self.players[player.id] = player.username
         return True
+
+    def start(self, args):
+        if self.started:
+            raise Exception("Игра уже началась")
+        if len(self.players) < MIN_PLAYERS:
+            raise Exception("Слишком мало игроков")
+        try:
+            self.rounds = int(args[0])
+            assert(self.rounds > 0)
+        except Exception as e:
+            raise Exception("Укажите число кругов, например так: /go 2")
+        self.started = True
--- main.py
+++ main.py
@@ -13,9 +13,17 @@ def help(update, context):
 
 Я знаю такие команды:
 /help - показать это сообщение
-/new - начать новую игру
+/new - начать регистрацию на новую игру
+/go 2 - стартовать игру, 2 круга
     """
     context.bot.send_message(chat_id=update.effective_chat.id, text=HELP_MESSAGE)
+
+
+def check_game_exists(context, update):
+    if 'game' not in context.chat_data:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Игра не начата. Чтобы начать игру, скажите /new")
+        return False
+    return True
 
 
 def new_game(update, context):
@@ -24,11 +32,22 @@ def new_game(update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, text="Начинаем новую игру. Чтобы зарегистрироваться, напишите '+', 'да' или 'я'")
 
 
+def start_game(update, context):
+    if check_game_exists(context, update):
+        game = context.chat_data['game']
+        if not game.started:
+            try:
+                game.start(context.args)
+                players_list = ', '.join('@{}'.format(u) for u in game.players.values())
+                context.bot.send_message(chat_id=update.effective_chat.id, text="Внимание, начинаем игру. Игроки: {}. Кругов: {}".format(players_list, game.rounds))
+            except Exception as e:
+                context.bot.send_message(chat_id=update.effective_chat.id, text="Не удалось начать игру: {}".format(e))
+
+
 def register(update, context):
-    if 'game' not in context.chat_data:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Игра не начата. Чтобы начать игру, скажите /new")
-    if context.chat_data['game'].add_player(update.message.from_user.id):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Записал, @{}".format(update.message.from_user.username))
+    if check_game_exists(context, update):
+        if context.chat_data['game'].add_player(update.message.from_user):
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Записал, @{}".format(update.message.from_user.username))
 
 
 def read_token():
@@ -43,9 +62,9 @@ def main():
     updater = Updater(token=read_token(), use_context=True)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler('start', help))
-    dispatcher.add_handler(CommandHandler('help', help))
+    dispatcher.add_handler(CommandHandler(['start', 'help'], help))
     dispatcher.add_handler(CommandHandler('new', new_game))
+    dispatcher.add_handler(CommandHandler('go', start_game))
     dispatcher.add_handler(MessageHandler(Filters.text(['+', 'да', 'я']), register))
 
     updater.start_polling()
