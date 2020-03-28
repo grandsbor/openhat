cc28e824ae579197ac4b7788c7e299fde5fb87c3
Dmitry Granovsky <dima.granovsky@gmail.com>
2020-03-28 16:58:39 +0300

    most of game loop

--- game.py
+++ game.py
@@ -10,6 +10,7 @@ class Game():
         self.players = {}
         self.started = False
         self.rounds = 0
+        self.turn_generator = None
 
     @staticmethod
     def init(self, dict_path):
@@ -21,19 +22,33 @@ class Game():
         self.players[player.id] = player.username
         return True
 
-    def next_turn(self):
+    def generate_turns(self, rounds):
+        logging.debug("call generate_turns()")
         assert(self.started)
         people = list(self.players.items())
-        return [(people[0], people[-1])]  # XXX temp
+        # XXX temp
+        for pair in [(people[0], people[-1]), (people[-1], people[0])]:
+            yield pair
         """
         n = len(people)
-        for j in range(1, n):
-            for i in range(n):
-                yield (people[i], people[(i + j) % n])
+        for _ in range(rounds):
+            for j in range(1, n):
+                for i in range(n):
+                    yield (people[i], people[(i + j) % n])
         """
 
+
+    def next_turn(self):
+        return next(self.turn_generator)
+
+    def log_explained(self, explainer, guesser, word):
+        pass
+
+    def log_skipped(self, explainer, word):
+        pass
+
     def next_word(self, player):
-        return ""
+        return "КРОКОДИЛ"
 
     def start(self, args):
         if self.started:
@@ -46,6 +61,7 @@ class Game():
         except Exception as e:
             raise Exception("Укажите число кругов, например так: /go 2")
         self.started = True
+        self.turn_generator = self.generate_turns(self.rounds)
 
     def finish(self):
         self.started = False
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import logging
-from telegram import KeyboardButton, ReplyKeyboardMarkup
+from telegram import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, CommandHandler, MessageHandler
 from telegram.ext.filters import Filters
 
@@ -10,6 +10,8 @@ from game import Game
 
 
 START_BUTTON_TEXT = 'Поехали'
+WORD_EXPLAINED_BUTTON_TEXT = 'Отгадано'
+WORD_SKIPPED_BUTTON_TEXT = 'Пропустить'
 
 
 def help(update, context):
@@ -25,52 +27,85 @@ def help(update, context):
 
 
 def check_game_exists(context, update):
-    if 'game' not in context.chat_data:
+    if 'game' not in context.bot_data:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Игра не начата. Чтобы начать игру, скажите /new")
         return False
     return True
 
 
+def broadcast(context, msg):
+    context.bot.send_message(chat_id=context.bot_data['common_chat_id'], text=msg)
+
+
 def new_game(update, context):
     # TODO check that chat is not 1-on-1 with bot
-    if 'game' not in context.chat_data:
-        context.chat_data['game'] = Game()
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Начинаем новую игру. Чтобы зарегистрироваться, напишите '+', 'да' или 'я'")
+    if 'game' not in context.bot_data:
+        chat_id = update.effective_chat.id
+        context.bot_data['game'] = Game()
+        context.bot_data['common_chat_id'] = chat_id
+        context.bot.send_message(chat_id, text="Начинаем новую игру. Чтобы зарегистрироваться, напишите /reg")
 
 
 def register(update, context):
     if check_game_exists(context, update):
-        if context.chat_data['game'].add_player(update.message.from_user):
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Записал, @{}".format(update.message.from_user.username))
+        if context.bot_data['game'].add_player(update.message.from_user):
+            broadcast(context, "Записал, @{}".format(update.message.from_user.username))
+
+
+def next_turn(context):
+    try:
+        game = context.bot_data['game']
+        explainer, guesser = game.next_turn()
+        context.bot_data['explainer'] = explainer[0]
+        broadcast(context, "Сейчас объясняет @{}, отгадывает @{}".format(explainer[1], guesser[1]))
+        context.bot.send_message(chat_id=explainer[0],
+                                 text="Твоя очередь объяснять, отгадывает @{}. Нажми кнопку по готовности.".format(guesser[1]),
+                                 reply_markup=ReplyKeyboardMarkup.from_button(START_BUTTON_TEXT, resize_keyboard=True, one_time_keyboard=True),
+                                )
+    except StopIteration:
+        finish_game(context)
 
 
 def start_game(update, context):
-    def broadcast(msg):
-        context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
-
     if check_game_exists(context, update):
-        game = context.chat_data['game']
+        game = context.bot_data['game']
         if game.started:
             return
         try:
             game.start(context.args)
             players_list = ', '.join('@{}'.format(u) for u in game.players.values())
-            broadcast("Внимание, начинаем игру. Игроки: {}. Кругов: {}".format(players_list, game.rounds))
+            broadcast(context, "Внимание, начинаем игру. Игроки: {}. Кругов: {}".format(players_list, game.rounds))
+            next_turn(context)
         except Exception as e:
-            broadcast("Не удалось начать игру: {}".format(e))
-
-        for explainer, guesser in game.next_turn():
-            broadcast("Сейчас объясняет @{}, отгадывает @{}".format(explainer[1], guesser[1]))
-            context.bot.send_message(chat_id=explainer[0],
-                                     text="Твоя очередь объяснять, отгадывает @{}. Нажми кнопку по готовности.".format(guesser[1]),
-                                     reply_markup=ReplyKeyboardMarkup.from_button(START_BUTTON_TEXT, resize_keyboard=True, one_time_keyboard=True),
-                                    )
+            broadcast(context, "Не удалось начать игру: {}".format(e))
 
 
-def start_explain(update, context):
-    # TODO check that correct player pushed the button
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Здесь будут появляться слова и запускаться таймер.")
-    context.game.finish()
+def finish_game(context):
+    game = context.bot_data['game']
+    stats = game.finish()
+    broadcast(context, "Всем спасибо, игра окончена.")
+    # TODO write some stats
+    del context.bot_data['game']
+
+
+def end_explain(context):
+    context.bot.send_message(context.job.context, text="Кончилось твоё время!", reply_markup=ReplyKeyboardRemove())
+    # TODO hide buttons
+    next_turn(context)
+
+
+def explain_cb(update, context):
+    # TODO check that current player is correct
+    chat_id = update.effective_chat.id
+    # the main part is here
+    # TODO log ok/skip if present
+    game = context.bot_data['game']
+    word = game.next_word(chat_id)
+    context.bot.send_message(chat_id,
+                             text="Ваше слово - {}".format(word),
+                             reply_markup=ReplyKeyboardMarkup([[WORD_EXPLAINED_BUTTON_TEXT, WORD_SKIPPED_BUTTON_TEXT]], resize_keyboard=True, one_time_keyboard=True))
+    if len(context.job_queue.jobs()) == 0:
+        context.job_queue.run_once(end_explain, 10, context=chat_id)
 
 
 def read_token(token):
@@ -88,8 +123,8 @@ def main(token):
     dispatcher.add_handler(CommandHandler(['start', 'help'], help))
     dispatcher.add_handler(CommandHandler('new', new_game))
     dispatcher.add_handler(CommandHandler('go', start_game))
-    dispatcher.add_handler(MessageHandler(Filters.text(['+', 'да', 'я']), register))
-    dispatcher.add_handler(MessageHandler(Filters.text(START_BUTTON_TEXT), start_explain))
+    dispatcher.add_handler(CommandHandler('reg', register))
+    dispatcher.add_handler(MessageHandler(Filters.text([START_BUTTON_TEXT, WORD_EXPLAINED_BUTTON_TEXT, WORD_SKIPPED_BUTTON_TEXT]), explain_cb))
 
     updater.start_polling()
     updater.idle()
